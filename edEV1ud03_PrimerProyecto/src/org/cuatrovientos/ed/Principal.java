d1fa8f496d84e70e4f34d92c5c08e91c969743e0
AlexMeleiro <alexmele428@gmail.com>
2022-11-15 10:42:22 +0100

    Create Project

---
+++ edEV1ud03_PrimerProyecto/src/org/cuatrovientos/ed/Principal.java
@@ -0,0 +1,14 @@
+package org.cuatrovientos.ed;
+
+public class Principal {
+	static int i = 0;
+	public static void main(String[] args) {
+			//Station int i = 0;
+			//int i = 0;
+			System.out.println("prueba1");
+			i = i+1;
+			System.out.println("Prueba2");
+
+	}
+
+}
